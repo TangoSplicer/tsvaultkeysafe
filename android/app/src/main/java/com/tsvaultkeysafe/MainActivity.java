08a44abfb36985e0e281df679625fd0ff53ba556
TangoSplicer <jasonmaoakley@gmail.com>
2026-01-05 14:06:21 +0000

    Create MainActivity.java

---
+++ android/app/src/main/java/com/tsvaultkeysafe/MainActivity.java
@@ -0,0 +1,10 @@
+package com.tsvaultkeysafe;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "tsvaultkeysafe";
+    }
+}
